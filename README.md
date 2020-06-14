54d3189c2270a7412387bad9941e441851d93151
andresma2490 <montenegro.andres@correounivalle.edu.co>
2020-06-14 13:07:11 -0500

    FaaS funcion que consulta los datos relacionados al COVID-19 en el pais y reduce los resultados a informacion especifica del Valle del Cauca y Cali

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pyPandas
+# pyFaaS
 
 ## Instrucciones
 
--- pyfunction/handler.py
+++ pyfunction/handler.py
@@ -1,10 +1,28 @@
 import numpy as np
 import pandas as pd
+import requests
+import io
 
-def handle(req):	
-	"""handle a request to the function
-    Args:
-        req (str): request body
-    """
-
-	return req
+def handle(req):
+	url = 'https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessType=DOWNLOAD'
+	
+	try:
+	  print("Descargando los datos")
+	  urlData = requests.get(url).content
+	
+	except:
+	  print("Hubo un error, intentelo de nuevo")
+	  return
+	
+	df = pd.read_csv(io.StringIO(urlData.decode('utf-8')), usecols=['Departamento o Distrito ', 'Ciudad de ubicación', 'atención', 'Edad', 'Tipo'])
+	df = df.rename(columns = {'Departamento o Distrito ':'Departamento', 'Ciudad de ubicación':'Ciudad'})
+	df = df.loc[ df.Departamento == 'Valle del Cauca' ]
+	
+	print("\nCasos de COVID-19 en el Valle del Cauca")
+	print(df.groupby('Ciudad').Ciudad.count())
+	
+	print("\nDe los cuales en Cali son del tipo...")
+	df = df.loc[ df.Ciudad == 'Cali' ]
+	print(df.groupby('Tipo').Tipo.count())
+	
+	return 
